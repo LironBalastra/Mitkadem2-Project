64a7d1b55e3161a6577fdf8074fc7842e846359a
Amit <amit_cohen@systemglob.com>
2022-04-16 13:54:06 +0300

    mic and video init

--- src/components/ChatScreen.js
+++ src/components/ChatScreen.js
@@ -72,9 +72,8 @@ function ChatScreen() {
 								  </div>
 								  <div class="col-lg-6 hidden-sm text-right">
 									  <a href="javascript:void(0);" class="btn btn-outline-secondary "><i class="fa fa-camera"></i></a>
-									  <a href="javascript:void(0);" class="btn btn-outline-primary"><i class="fa fa-image"></i></a>
-									  <a href="javascript:void(0);" class="btn btn-outline-info"><i class="fa fa-cogs"></i></a>
-									  <a href="javascript:void(0);" class="btn btn-outline-warning"><i class="fa fa-question"></i></a>
+									  <a href="javascript:void(0);" class="btn btn-outline-primary"><i class="fa fa-youtube-play"></i></a>
+									  <a href="javascript:void(0);" class="btn btn-outline-success"><i class="fa fa-microphone"></i></a>
 								  </div>
 							  </div>
 						  </div>
